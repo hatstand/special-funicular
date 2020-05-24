45c3b2f76dbc52d4a803b0c015abe79c00e41390
John Maguire <john.maguire@gmail.com>
2020-05-24 01:34:30 +0100

    Hacky rotation

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 const itemsUrls: string[] = [
   "/items/banana.png",
-  "/items/lightning.png",
   "/items/blue-shell.png",
+  "/items/lightning.png",
   "/items/mushroom.png",
+  "/items/red-shell.png",
   "/items/star.png",
 ];
 
 function App() {
+  const [itemUrl, setItemUrl] = useState("/items/item-box.png");
+  const [isRunning, setRunning] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setItemUrl(itemsUrls[Math.floor(Math.random() * itemsUrls.length)]);
+    }, isRunning ? 250 : 10000000000);
+    return () => clearInterval(interval);
+  });
+
+  const onLeader = () => {
+    setRunning(true);
+  };
+
   return (
     <div className="App">
-      <img src="/items/banana.png" width="400" height="400" />
+      <div>{`Running: ${isRunning}`}</div>
+      <img src={itemUrl} width="400" height="400" />
+      <button onClick={onLeader}>Leader</button>
+      <button>Mid Pack</button>
+      <button>Bottom Two</button>
     </div>
   );
 }
